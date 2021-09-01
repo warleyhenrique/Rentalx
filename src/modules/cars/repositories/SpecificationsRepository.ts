1575c139b51e76c641faded2476ae21bf29a722b
warleyhenrique <warley.henrique93@gmail.com>
2021-09-01 00:00:38 -0300

    Create Repository and Services for Categories and Specifications

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,32 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationsDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find((spc) => spc.name === name);
+    return specification;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  create({ name, description }: ICreateSpecificationsDTO): void {
+    const specification = new Specification();
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+}
+
+export { SpecificationRepository };
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificationRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+/*
+specificationsRoutes.get("/", (request, response) => {
+  const all = specificationsRepository.list();
+
+  return response.json(all);
+});
+*/
+export { specificationsRoutes };
